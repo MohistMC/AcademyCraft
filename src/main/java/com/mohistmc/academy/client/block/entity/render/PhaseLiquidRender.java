050f13feb36f575df9b3a9ca5c297b3e60eda021
lliioollcn <lliiooll@126.com>
2023-04-01 18:17:42 +0800

    绘制偏移，并且是静态的

--- src/main/java/com/mohistmc/academy/client/block/entity/render/PhaseLiquidRender.java
+++ src/main/java/com/mohistmc/academy/client/block/entity/render/PhaseLiquidRender.java
@@ -4,17 +4,18 @@ import com.mohistmc.academy.client.block.entity.PhaseLiquidBlockEntity;
 import com.mohistmc.academy.client.block.entity.model.CatEngineModel;
 import com.mohistmc.academy.utils.Resources;
 import com.mojang.blaze3d.platform.GlStateManager;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.BufferUploader;
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.LiquidBlockRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import org.joml.Matrix4f;
+import org.joml.Vector4f;
 import org.lwjgl.opengl.GL11;
 
 public class PhaseLiquidRender implements BlockEntityRenderer<PhaseLiquidBlockEntity> {
@@ -41,31 +42,42 @@ public class PhaseLiquidRender implements BlockEntityRenderer<PhaseLiquidBlockEn
         GlStateManager._depthMask(false);
         GlStateManager._disableDepthTest();
         GlStateManager._disableCull();
+        RenderSystem.setShaderColor(1, 1, 1, alpha);
         double ht = 1.2 * Math.sqrt(
                 3
         );
         GlStateManager._enableBlend();
-        drawLayer(souce, 0, -0.3 * ht, 0.3f, 0.2f, 0.7f, alpha);
-        drawLayer(souce, 1, 0.35 * ht, 0.3f, 0.05f, 0.7f, alpha);
+        drawLayer(p_112309_, p, 0, (float) (-0.3 * ht), 0.3f, 0.2f, 0.7f, alpha);
+        drawLayer(p_112309_, p, 1, (float) (0.35 * ht), 0.3f, 0.05f, 0.7f, alpha);
         if (ht > 0.5)
-            drawLayer(souce, 2, 0.7 * ht, 0.1f, 0.25f, 0.7f, alpha);
+            drawLayer(p_112309_, p, 2, (float) (0.7 * ht), 0.1f, 0.25f, 0.7f, alpha);
         GlStateManager._depthMask(true);
         GlStateManager._enableDepthTest();
         GlStateManager._enableCull();
         p_112309_.popPose();
     }
 
-    private void drawLayer(MultiBufferSource souce, int layer, double height, float vx, float vz, float density, float alpha) {
+    private void drawLayer(PoseStack stack, BlockPos p, int layer, float height, float vx, float vz, float density, float alpha) {
         float time = System.currentTimeMillis();
         float du = (time * vx) % 1;
         float dv = (time * vz) % 1;
         ResourceLocation location = layers[layer];
-        BufferBuilder consumer = (BufferBuilder) souce.getBuffer(RenderType.entityTranslucent(location));
-        consumer.vertex(0, height, 0).color(1f, 1f, 1f, alpha).uv(du, dv).uv2(15728880).endVertex();
-        consumer.vertex(1, height, 0).color(1f, 1f, 1f, alpha).uv(du + density, dv).uv2(15728880).endVertex();
-        consumer.vertex(1, height, 1).color(1f, 1f, 1f, alpha).uv(du + density, dv + density).uv2(15728880).endVertex();
-        consumer.vertex(0, height, 1).color(1f, 1f, 1f, alpha).uv(du, dv + density).uv2(15728880).endVertex();
-        BufferUploader.draw(consumer.end());
+        RenderSystem.setShaderTexture(0, location);
+        Matrix4f pos = stack.last().pose();
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        BufferBuilder consumer = Tesselator.getInstance().getBuilder();
+        int x = p.getX(), y = p.getY();
+        consumer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
+        addVertex(pos, consumer, 1f, 68, 0f, du, dv);
+        addVertex(pos, consumer, 1f, 68, 0f, du + density, dv);
+        addVertex(pos, consumer, 1f, 68, 1f, du + density, dv + density);
+        addVertex(pos, consumer, 0f, 68, 1f, du, dv + density);
+        BufferUploader.drawWithShader(consumer.end());
 
+    }
+
+    private void addVertex(Matrix4f pos, BufferBuilder consumer, float x, float y, float z, float du, float dv) {
+        Vector4f p = pos.transform(new Vector4f(x, y, z, 1.0f));
+        consumer.vertex(p.x(), p.y(), p.z()).uv(du, dv).endVertex();
     }
 }
