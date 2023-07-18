044340268d66f4609807651ef831e3a65c319ed7
lliioollzh <lliiooll@126.com>
2023-07-18 20:50:24 +0800

    加密节点列表

--- src/main/java/com/mohistmc/academy/capability/AcademyNode.java
+++ src/main/java/com/mohistmc/academy/capability/AcademyNode.java
@@ -6,15 +6,21 @@ import net.minecraft.world.level.Level;
 public class AcademyNode {
 
     private String name = "Unnamed";
+    private String pass = "";
     private BlockPos pos;
     private Level level;
     private boolean authed = false;
 
-    public AcademyNode(String name, BlockPos pos, Level level, boolean authed) {
+    public AcademyNode(String name, String pass, BlockPos pos, Level level) {
         this.name = name;
         this.pos = pos;
+        this.pass = pass;
         this.level = level;
-        this.authed = authed;
+        this.authed = !pass.isBlank();
+    }
+
+    public AcademyNode(String name, BlockPos pos, Level level) {
+        this(name, "", pos, level);
     }
 
     public String getName() {
@@ -23,6 +29,14 @@ public class AcademyNode {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
     }
 
     public BlockPos getPos() {
@@ -41,11 +55,15 @@ public class AcademyNode {
         this.level = level;
     }
 
-    public boolean isAuthed() {
+    public boolean isNeedAuth() {
         return authed;
     }
 
-    public void setAuthed(boolean authed) {
-        this.authed = authed;
+    public void needAuth() {
+        this.authed = true;
+    }
+
+    public void noNeedAuth() {
+        this.authed = false;
     }
 }
