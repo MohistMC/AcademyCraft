232910bd3b55d3b4b3a97d4ce4dea351daefa5ef
Mgazul <mgazul@163.com>
2023-12-18 01:23:09 +0800

    actions test

--- src/main/java/cn/academy/AcademyCraft.java
+++ src/main/java/cn/academy/AcademyCraft.java
@@ -47,7 +47,7 @@ public class AcademyCraft {
     @Instance("academy-craft")
     public static AcademyCraft INSTANCE;
 
-    public static final String VERSION = "@VERSION@";
+    public static final String VERSION = "1.1.5";
 
     public static final boolean DEBUG_MODE = VERSION.startsWith("@");
 
