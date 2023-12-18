5912167ea9598561bb6dcff2291da88e315e57d0
Mgazul <mgazul@163.com>
2023-12-18 17:49:31 +0800

    1.1.6

--- src/main/java/cn/academy/AcademyCraft.java
+++ src/main/java/cn/academy/AcademyCraft.java
@@ -47,7 +47,7 @@ public class AcademyCraft {
     @Instance("academy-craft")
     public static AcademyCraft INSTANCE;
 
-    public static final String VERSION = "1.1.5";
+    public static final String VERSION = "1.1.6";
 
     public static final boolean DEBUG_MODE = VERSION.startsWith("@");
 
