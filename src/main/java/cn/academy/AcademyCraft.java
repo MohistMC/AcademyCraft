927cfea90f42405ad2f8fede835c9cce4f2a1682
Mgazul <mgazul@163.com>
2026-08-30 16:20:24 +0800

    start 1.1.8

--- src/main/java/cn/academy/AcademyCraft.java
+++ src/main/java/cn/academy/AcademyCraft.java
@@ -46,7 +46,7 @@ public class AcademyCraft {
     @Instance("academy-craft")
     public static AcademyCraft INSTANCE;
 
-    public static final String VERSION = "1.1.7";
+    public static final String VERSION = "1.1.8";
 
     public static final boolean DEBUG_MODE = Boolean.getBoolean("academy.debug");
 
